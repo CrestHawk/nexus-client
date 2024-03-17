8f670a709e1ade8cad53a0734a4658d5d178b3a1
Tom <emailtomdavies@gmail.com>
2024-03-17 18:19:49 +0000

    Added state for tracking selected list item

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,10 +1,14 @@
-import React, { MouseEvent } from "react";
+import React, { useState } from "react";
 
 function ListGroup() {
     let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
-    // Event handler
-    const handleClick = (event: MouseEvent) => console.log(event)
+    // Use state returns an array.
+    // The first element in the returned array is the state value that represents the current state
+    // The second element is a reference to the state setter function
+    // So here we are using Typescript destructuring to grab the 2 references returned from useState and then assign them to individual
+    // variables called selectedIndex and setSelectedIndex
+    const [selectedIndex, setSelectedIndex] = useState(-1);
 
     return (
         // This <> is a Fragment in react. Use this when you need to wrap multiple elements for your component to return
@@ -15,9 +19,15 @@ function ListGroup() {
             <ul className="list-group">
                 {items.map((item, index) => (
                     <li
-                        className="list-group-item"
+                        className={
+                            selectedIndex === index
+                                ? "list-group-item active"
+                                : "list-group-item"
+                        }
                         key={item}
-                        onClick={handleClick}
+                        onClick={() => {
+                            setSelectedIndex(index);
+                        }}
                     >
                         {item}
                     </li>
